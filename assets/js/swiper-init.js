070a4e6af9158b03383025603410cfe43b10cac2
Juank648 <jkam86425@gmail.com>
2025-05-13 00:16:06 -0500

    material educativo

---
+++ assets/js/swiper-init.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
